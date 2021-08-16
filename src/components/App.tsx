76a1ab3de9c7b5f1c45a4a17e757eb2b4a07765f
cothis <minho1030@naver.com>
2021-08-16 16:24:37 +0900

    feat: constructWithTag

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,16 @@
 import styled from '../lib/styled-components/styled-components';
 
 const Div = styled.div``;
+const Button = styled.button``;
+const A = styled.a``;
 
 const App = () => {
   return (
     <div>
       React Boiler Plate
       <Div>
-        <div>하이하이</div>
-        <div>second</div>
+        <Button>버튼</Button>
+        <A href="https://www.naver.com">앵커태그</A>
       </Div>
     </div>
   );
--- src/lib/styled-components/styled-components.tsx
+++ src/lib/styled-components/styled-components.tsx
@@ -3,15 +3,28 @@ interface Props {
   children: ReactNode;
 }
 
-const construct = (strings: TemplateStringsArray, ...args: any[]): Function => {
-  const NewComponent = (props: Props) => {
-    return <div>{props.children}</div>;
+const constructWithTag = (tag: string) => {
+  const CustomTag = `${tag}` as keyof JSX.IntrinsicElements;
+
+  const construct = (
+    strings: TemplateStringsArray,
+    ...args: any[]
+  ): Function => {
+    const NewComponent = (props: Props) => {
+      return <CustomTag {...props}>{props.children}</CustomTag>;
+    };
+    return NewComponent;
   };
-  return NewComponent;
+
+  return construct;
 };
 
 const styled = {
-  div: construct,
+  div: constructWithTag('div'),
+  span: constructWithTag('span'),
+  a: constructWithTag('a'),
+  form: constructWithTag('form'),
+  button: constructWithTag('button'),
 };
 
 export default styled;
